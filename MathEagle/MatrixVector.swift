db523885f055b4b393fc7ea63616ad7a457b3126
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-03-31 15:14:00 +0200

    Products between vector and matrix

--- MathEagle/MatrixVector.swift
+++ MathEagle/MatrixVector.swift
@@ -10,7 +10,48 @@ import Foundation
 
 // MARK: Vector - Matrix Product
 
-func * <T: FullMathValue> (left: Vector<T>, right: Matrix<T>) -> Vector<T> {
+
+func * <T: MatrixCompatible> (left: Vector<T>, right: Matrix<T>) -> Vector<T> {
     
-    return Vector()
+    if left.length != right.dimensions.rows {
+        
+        NSException(name: "Unequal dimensions", reason: "The vector's length (\(left.length)) is not equal to the matrix's number of rows (\(right.dimensions.rows)).", userInfo: nil).raise()
+    }
+    
+    if left.length == 0 { return Vector() }
+    
+    var elements = [T]()
+    var i = 0
+    
+    while i < left.length {
+        
+        elements.append(left.dotProduct(right.column(i)))
+        
+        i++
+    }
+    
+    return Vector(elements)
+}
+
+
+func * <T: MatrixCompatible> (left: Matrix<T>, right: Vector<T>) -> Vector<T> {
+    
+    if right.length != left.dimensions.columns {
+        
+        NSException(name: "Unequal dimensions", reason: "The vector's length (\(right.length)) is not equal to the matrix's number of columns (\(left.dimensions.columns)).", userInfo: nil).raise()
+    }
+    
+    if right.length == 0 { return Vector() }
+    
+    var elements = [T]()
+    var i = 0
+    
+    while i < right.length {
+        
+        elements.append(right.dotProduct(left.row(i)))
+        
+        i++
+    }
+    
+    return Vector(elements)
 }
---
+++ MathEagleTests/MatrixVectorTests.swift
@@ -0,0 +1,41 @@
+//
+//  MatrixVectorTests.swift
+//  MathEagle
+//
+//  Created by Rugen Heidbuchel on 31/03/15.
+//  Copyright (c) 2015 Jorestha Solutions. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MatrixVectorTests: XCTestCase {
+
+    func testVectorMatrixProduct() {
+        
+        var vector = Vector([1, 2, 3])
+        var matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        
+        XCTAssertEqual(Vector([30, 36, 42]), vector * matrix)
+        
+        vector = Vector()
+        matrix = Matrix()
+        
+        XCTAssertEqual(Vector<Int>(), vector * matrix)
+    }
+    
+    
+    func testMatrixVectorProduct() {
+        
+        var vector = Vector([1, 2, 3])
+        var matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        
+        XCTAssertEqual(Vector([14, 32, 50]), matrix * vector)
+        
+        vector = Vector()
+        matrix = Matrix()
+        
+        XCTAssertEqual(Vector<Int>(), matrix * vector)
+    }
+
+}
