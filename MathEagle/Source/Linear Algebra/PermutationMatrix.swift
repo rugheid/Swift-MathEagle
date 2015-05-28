871d7ca34abc6922fdbd828f2ebb4cf9d52f940c
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-05-28 10:43:38 +0200

    PermutationMatrix element methods

--- MathEagle/Source/Linear Algebra/PermutationMatrix.swift
+++ MathEagle/Source/Linear Algebra/PermutationMatrix.swift
@@ -41,6 +41,7 @@ public class PermutationMatrix <T: MatrixCompatible> : Matrix<T> {
     }
     
     
+    
     // MARK: Initialisers
     
     /**
@@ -53,5 +54,176 @@ public class PermutationMatrix <T: MatrixCompatible> : Matrix<T> {
         self.permutation = permutation
         
         super.init()
+    }
+    
+    
+    
+    // MARK: Basic Properties
+    
+    /**
+        Returns a row major ordered list of all elements in the array.
+        This should be used for high performance applications.
+    */
+    override public var elementsList: [T] {
+        
+        get {
+            
+            let i = self.size
+            
+            var elementsList = [T](count: i * i, repeatedValue: 0)
+            for r in 0 ..< i {
+                elementsList[r * i + self.permutation[r]] = 1
+            }
+            
+            return elementsList
+        }
+        
+        set (newElementsList) {
+            //TODO: Implement this.
+        }
+    }
+    
+    
+    /**
+        Returns the size of the matrix if the matrix is square. If the matrix is not
+        square it returns nil.
+    
+        :returns: The size of the matrix if the matrix is square or nil otherwise.
+    */
+    override public var size: Int {
+        return self.permutation.length
+    }
+    
+    
+    
+    // MARK: Element Methods
+    
+    /**
+        Returns the element at the given index (row, column).
+    
+        :param: row     The row index of the requested element
+        :param: column  The column index of the requested element
+    
+        :returns: The element at the given index (row, column).
+    */
+    override public func element(row: Int, _ column: Int) -> T {
+        
+        if row < 0 || row >= self.size {
+            
+            NSException(name: "Row index out of bounds", reason: "The requested element's row index is out of bounds.", userInfo: nil).raise()
+        }
+        
+        if column < 0 || column >= self.size {
+            
+            NSException(name: "Column index out of bounds", reason: "The requested element's column index is out of bounds.", userInfo: nil).raise()
+        }
+        
+        return self.permutation[row] == column ? 1 : 0
+    }
+    
+    
+    /**
+        Sets the element at the given indexes.
+    
+        :param: row     The row index of the element
+        :param: column  The column index of the element
+        :param: element The element to set at the given indexes
+    */
+    override public func setElement(atRow row: Int, atColumn column: Int, toElement element: T) {
+        
+        NSException(name: "PermutationMatrix is unsettable", reason: "You can't directly set the elements of a permutation matrix.", userInfo: nil).raise()
+    }
+    
+    
+    /**
+        Returns the row at the given index. The first row has index 0.
+    
+        :returns:   The row at the given index.
+    */
+    override public func row(index: Int) -> Vector<T> {
+        
+        if index < 0 || index >= self.size {
+            
+            NSException(name: "Row index out of bounds", reason: "The requested row's index is out of bounds.", userInfo: nil).raise()
+        }
+        
+        var elementsList = [T](count: self.size, repeatedValue: 0)
+        
+        elementsList[index] = 1
+        
+        return Vector(elementsList)
+    }
+    
+    
+    /**
+        Sets the row at the given index to the given row.
+    
+        :param: index   The index of the row to change.
+        :param: newRow  The row to set at the given index.
+    */
+    override public func setRow(atIndex index: Int, toRow newRow: Vector<T>) {
+        
+        NSException(name: "PermutationMatrix is unsettable", reason: "You can't directly set the elements of a permutation matrix.", userInfo: nil).raise()
+    }
+    
+    
+    /**
+        Switches the rows at the given indexes.
+    
+        :param: i   The index of the first row.
+        :param: j   The index of the second row.
+    */
+    override public func switchRows(i: Int, _ j: Int) {
+        
+        if i < 0 || i >= self.size || j < 0 || j >= self.size {
+            
+            NSException(name: "Row index out of bounds", reason: "The index of the row that should be switched is out of bounds.", userInfo: nil).raise()
+        }
+        
+        self.permutation.switchElements(i, j)
+    }
+    
+    
+    /**
+        Returns the column at the given index. The first column has index 0.
+    
+        :returns: The column at the given index.
+    */
+    override public func column(index: Int) -> Vector<T> {
+        
+        if index < 0 || index >= self.size {
+            
+            NSException(name: "Column index out of bounds", reason: "The requested column's index is out of bounds.", userInfo: nil).raise()
+        }
+        
+        var column = [T](count: self.size, repeatedValue: 0)
+        
+        column[self.permutation.indexOfElement(index)] = 1
+        
+        return Vector(column)
+    }
+    
+    
+    /**
+        Sets the column at the given index to the given column.
+    
+        :param: index   The index of the column to change.
+        :param: column  The column to set at the given index.
+    */
+    override public func setColumn(atIndex index: Int, toColumn newColumn: Vector<T>) {
+        
+        NSException(name: "PermutationMatrix is unsettable", reason: "You can't directly set the elements of a permutation matrix.", userInfo: nil).raise()
+    }
+    
+    
+    /**
+        Switches the columns at the given indexes.
+    
+        :param: i   The index of the first column.
+        :param: j   The index of the second column.
+    */
+    override public func switchColumns(i: Int, _ j: Int) {
+        
+        //TODO: Implement this
     }
 }
