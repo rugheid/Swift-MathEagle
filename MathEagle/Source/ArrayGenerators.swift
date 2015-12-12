07ffccb8cf3802ab449910f464d24f9a6edd1fba
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2015-12-12 18:12:34 +0100

    Update rampedArray comment

--- MathEagle/Source/ArrayGenerators.swift
+++ MathEagle/Source/ArrayGenerators.swift
@@ -10,11 +10,15 @@ import Foundation
 
 
 /**
-    Returns an array of the given length. The first element is the given initial value.
-    Following elements will be incremented by increment.
-
-    :example: rampedArray(initialValue: 2, increment: 3, length: 4) will return
-                [2, 5, 8, 11]
+ Returns an array of the given length. The first element is the given initial value.
+ Following elements will be incremented by increment.
+ 
+ - parameter length: The number of elements the array should have.
+ - parameter initialValue: The first value of the array. Default 0.
+ - parameter increment: The difference between two adjecent elements. Default 1.
+ 
+ :example: rampedArray(length: 4, initialValue: 2, increment: 3) will return
+ [2, 5, 8, 11]
 */
 public func rampedArray <T: protocol<IntegerLiteralConvertible, Addable>> (length length: Int, initialValue: T = 0, increment: T = 1) -> [T] {
     
